8e944432f300f94eef456b3c2bf645e400980268
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-18 10:12:59 -0400

    add continuous, needs tests still (#2)

--- R/tip.R
+++ R/tip.R
@@ -1,4 +1,4 @@
-#' Tipping point
+#' Tip a result with a binary confounder.
 #'
 #' choose two of the following three to specify, and the third will be estimated:
 #' * `p1`
@@ -16,49 +16,49 @@
 #'
 #' @examples
 #' #to output the size of an unmeasured confounder needed to tip analysis
-#' tip(p1 = .5,p0 = 0,lb = 1.2,ub = 1.5)
+#' tip_with_binary(p1 = .5, p0 = 0, lb = 1.2, ub = 1.5)
 #'
 #' #to output a sentence explanation
-#' tip(p1 = .5,p0 = 0,lb = 1.2,ub = 1.5,explanation = TRUE)
+#' tip_with_binary(p1 = .5, p0 = 0, lb = 1.2, ub = 1.5, explanation = TRUE)
 #'
 #' @export
-tip <- function(p1 = NULL, p0 = NULL, lb = NULL, ub = NULL, gamma = NULL, explanation = FALSE) {
+tip_with_binary <- function(p1 = NULL, p0 = NULL, gamma = NULL, lb = NULL, ub = NULL, explanation = FALSE) {
   if (lb > 1 & ub > 1){
-    b = lb
+    b <- lb
   } else if (lb < 1 & ub < 1){
-    b = ub
+    b <- ub
   } else stop("Please input a significant result.")
 
 
 
   if (is.null(gamma)){
 
-  if(p1 < 0 | p0 < 0 | p1 > 1 | p0 > 1) stop("The prevalences entered must be between 0 and 1")
+    if(p1 < 0 | p0 < 0 | p1 > 1 | p0 > 1) stop("The prevalences entered must be between 0 and 1")
 
-  gamma = ((1-p1)+b*(p0-1))/(b*p0-p1)
-  if ({
-    # !((p1/p0 > b & b > (1-p1)/(1-p0)) |
-    # (p1/p0 < b & b < (1-p1)/(1-p0)))
-    gamma < 0
-  }) {
+    gamma <- ((1-p1)+b*(p0-1))/(b*p0-p1)
+    if ({
+      # !((p1/p0 > b & b > (1-p1)/(1-p0)) |
+      # (p1/p0 < b & b < (1-p1)/(1-p0)))
+      gamma < 0
+    }) {
 
-    stop(sprintf("Given these prevelances, there does not exist an unmeasured confounder
-that could tip this. Please specify a larger prevalence difference
-(ie: make p0 and p1 farther apart)."))
-  }
-  if (explanation == TRUE){
-    cat(sprintf("An unmeasured confounder of size %s with a prevalence of %s in the exposed population and %s in the unexposed population would tip your (%s,%s) result to nonsignificance.",
-                round(gamma,2),p1, p0, lb, ub))
-    invisible(gamma)
+      stop(sprintf("Given these prevelances, there does not exist an unmeasured
+confounder that could tip this. Please specify a larger prevalence
+difference (ie: make p0 and p1 farther apart)."))
+    }
+    if (explanation == TRUE){
+      cat(sprintf("An unmeasured confounder of size %s with a prevalence of %s in the exposed population and %s in the unexposed population would tip your (%s,%s) result to nonsignificance.",
+                  round(gamma,2),p1, p0, lb, ub))
+      invisible(gamma)
     } else return(gamma)
   } else if (is.null(p0)){
 
     if(p1 < 0 | p1 > 1 ) stop("The prevalences entered must be between 0 and 1")
 
 
-    p0 = (p1*(-gamma)+p1+b-1)/(b-gamma*b)
+    p0 <- (p1*(-gamma)+p1+b-1)/(b-gamma*b)
     if (p0 > 1 | p0 < 0) {
-   stop(sprintf("Given these parameters, there does not exist an unmeasured confounder that could tip this."))
+      stop(sprintf("Given these parameters, there does not exist an unmeasured confounder that could tip this."))
     }
     if (explanation == TRUE){
       cat(sprintf("An unmeasured confounder of size %s with a prevalence of %s in the exposed population would need a prevalence of %s in the unexposed population to tip your (%s,%s) result to nonsignificance.",
@@ -70,7 +70,7 @@ that could tip this. Please specify a larger prevalence difference
     if(p0 < 0 | p0 > 1) stop("The prevalences entered must be between 0 and 1")
 
 
-    p1 = (b*((gamma-1)*p0+1)-1)/(gamma-1)
+    p1 <- (b*((gamma-1)*p0+1)-1)/(gamma-1)
     if (p1 > 1 | p1 < 0) {
       stop(sprintf("Given these parameters, there does not exist an unmeasured confounder that could tip this."))
     }
@@ -80,5 +80,64 @@ that could tip this. Please specify a larger prevalence difference
       invisible(p1)
     } else return(p1)
   } else warning("Please specify only 2 of the following: p0, p1, gamma. The function will return the third.")
-  }
+}
 
+#' Tip a result with a continuous confounder.
+#'
+#' choose one of the following, and the other will be estimated:
+#' * `mean_diff`
+#' * `gamma`
+#'
+#' @param mean_diff estimated mean difference of the unmeasured confounder in the
+#'  exposed population and unexposed population
+#' @param gamma estimated size of an unmeasured confounder
+#' @param lb lower bound of your observed effect
+#' @param ub upper bound of your observed effect
+#' @param explanation logical value indicates whether you'd like the function to
+#'  output a sentence explanation for inclusion in a manuscript.
+#'
+#' @return Numeric. The size of an unmeasured confounder at the given parameters
+#'  that would tip the observed result.
+#'
+#' @examples
+#' #to output the size of an unmeasured confounder needed to tip analysis
+#' tip_with_continuousy(mean_diff = -2, lb = 1.2,ub = 1.5)
+#'
+#' #to output a sentence explanation
+#' tip_with_continuous(mean_diff = -2, lb = 1.2,ub = 1.5,explanation = TRUE)
+#'
+#' @export
+tip_with_continuous <- function(mean_diff = NULL, gamma = NULL, lb = NULL, ub = NULL, explanation = FALSE) {
+  if (lb > 1 & ub > 1){
+    b <- lb
+  } else if (lb < 1 & ub < 1){
+    b <- ub
+  } else stop("Please input a significant result.")
+  if (is.null(gamma)){
+    gamma <- (1/b)^{1/(mean_diff)}
+
+    if (gamma < 0) {
+      stop("This might be wrong..")
+    }
+    if (explanation == TRUE){
+      cat(sprintf("An unmeasured confounder of size %s with a mean difference of %s between the exposed and unexposed population would tip your (%s,%s) result to nonsignificance.",
+                  round(gamma,2), mean_diff, lb, ub))
+      invisible(gamma)
+    } else return(gamma)
+  } else if (is.null(mean_diff)){
+    stop("we can't do this yet")
+  }
+}
+
+#' Tip a result with a binary confounder.
+#'
+#' Choose two of the following three to specify, and the third will be estimated:
+#' * `p1`
+#' * `p0`
+#' * `gamma`
+#' @inheritParams tip_with_binary
+#'
+#' @export
+tip_b <- function(p1 = NULL, p0 = NULL, gamma = NULL, lb = NULL, ub = NULL, explanation = FALSE) {
+  tip_with_binary(p1 = p1, p0 = p0, gamma = gamma, lb = lb, ub = ub, explanation = explanation)
+}
