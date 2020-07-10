58c0f6cfcfae7e9a3f7bcaaeafba1a9e1986e163
Lucy McGowan <lucydagostino@gmail.com>
2020-07-10 10:50:28 -0400

    update references

--- R/tipr-package.R
+++ R/tipr-package.R
@@ -2,6 +2,21 @@
 #'
 #' @name tipr
 #' @docType package
-#' @references Lin, D. Y., Psaty, B. M., & Kronmal, R. A. (1998). Assessing the sensitivity of regression results to unmeasured confounders in observational studies. Biometrics, 54(3), 948–963.
+#' @references
+#'
+#' D'Agostino McGowan, L. (2018). Improving Modern Techniques of Causal
+#' Inference: Finite Sample Performance of ATM and ATO Doubly Robust Estimators,
+#' Variance Estimation for ATO Estimators, and Contextualized Tipping Point
+#' Sensitivity Analyses for Unmeasured Confounding. PhD thesis, Vanderbilt
+#' University.
+#'
+#' VanderWeele, TJ, and Peng D (2017). Sensitivity Analysis in Observational
+#' Research: Introducing the E-Value. Ann Intern Med, 167(4), 268–74.
+#'
+#' Lin, DY, Psaty, BM, & Kronmal, RA. (1998). Assessing the sensitivity
+#' of regression results to unmeasured confounders in observational studies.
+#' Biometrics, 54(3), 948–963.
+#'
+#'
 #' @importFrom purrr %||%
 NULL
