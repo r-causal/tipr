825304472d030ff83a2a0da78a39c26e073e7ef3
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2022-09-05 09:00:16 -0400

    update package doc

--- R/tipr-package.R
+++ R/tipr-package.R
@@ -6,14 +6,15 @@
 #' @docType package
 #' @references
 #'
-#' D'Agostino McGowan, L. (2018). Improving Modern Techniques of Causal
-#' Inference: Finite Sample Performance of ATM and ATO Doubly Robust Estimators,
-#' Variance Estimation for ATO Estimators, and Contextualized Tipping Point
-#' Sensitivity Analyses for Unmeasured Confounding. PhD thesis, Vanderbilt
-#' University.
+#' D'Agostino McGowan, L, (2022). tipr: An R package for sensitivity analyses
+#' for unmeasured confounders. Journal of Open Source Software, 7(77), 4495.
 #'
 #' VanderWeele, TJ, and Peng D (2017). Sensitivity Analysis in Observational
 #' Research: Introducing the E-Value. Ann Intern Med, 167(4), 268–74.
+#'
+#' Cinelli, C, & Hazlett, C (2020). Making sense of sensitivity: Extending
+#' omitted variable bias. Journal of the Royal Statistical Society: Series B
+#' (Statistical Methodology), 82(1), 39–67.
 #'
 #' Lin, DY, Psaty, BM, & Kronmal, RA. (1998). Assessing the sensitivity
 #' of regression results to unmeasured confounders in observational studies.
